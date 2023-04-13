575153d7d4479487a86a8c39ff58bd95f3978969
kkwonju <kkwo4140@gmail.com>
2023-04-13 18:01:39 +0900

    게시글 수정, 삭제 기능 구현

--- src/main/java/com/KoreaIT/kkwo/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/kkwo/demo/controller/UsrArticleController.java
@@ -13,38 +13,38 @@ import com.KoreaIT.kkwo.demo.vo.Article;
 public class UsrArticleController {
 	int lastArticleId;
 	List<Article> articles;
-	
+
 	public UsrArticleController() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
-		
+
 		makeTestData();
 	}
-	
+
 	private void makeTestData() {
-		for(int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 10; i++) {
 			String title = "제목" + i;
-			String body = "내용"  + i;
-			
+			String body = "내용" + i;
+
 			writeArticle(title, body);
 		}
 	}
-	
+
 	public Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
-		
-		Article article	= new Article(id, title, body);
+
+		Article article = new Article(id, title, body);
 		articles.add(article);
 		lastArticleId++;
-		
+
 		return article;
 	}
-	
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 		int id = lastArticleId + 1;
-		Article article	= new Article(id, title, body);
+		Article article = new Article(id, title, body);
 		articles.add(article);
 		lastArticleId++;
 		return article;
@@ -54,5 +54,37 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articles;
+	}
+
+	public Article getArticleByInputedId(int id) {
+		for (Article article : articles) {
+			if(article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticleByInputedId(id);
+		if(article == null) {
+			return String.format("%d번 글은 존재하지 않습니다", id);
+		}
+		articles.remove(article);
+		return String.format("%d번 글이 삭제되었습니다", id);
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticleByInputedId(id);
+		if(article == null) {
+			return String.format("%d번 글은 존재하지 않습니다", id);
+		}
+		article.setTitle(title);
+		article.setBody(body);
+		return String.format("%d번 글이 수정되었습니다\n아이디: %d\n제목: %s\n내용: %s", id, article.getId(), article.getTitle(), article.getBody());
 	}
 }
--- src/main/java/com/KoreaIT/kkwo/demo/vo/Article.java
+++ src/main/java/com/KoreaIT/kkwo/demo/vo/Article.java
@@ -2,9 +2,11 @@ package com.KoreaIT.kkwo.demo.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Article {
 	private int id;
 	private String title;
