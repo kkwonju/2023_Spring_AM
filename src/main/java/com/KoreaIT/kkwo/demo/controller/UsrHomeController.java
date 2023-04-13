625b0bb9330c9de68064f24983f5dad8c0030de4
kkwonju <kkwo4140@gmail.com>
2023-04-13 15:37:08 +0900

    count값 초기화 및 증가

--- src/main/java/com/KoreaIT/kkwo/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/kkwo/demo/controller/UsrHomeController.java
@@ -6,19 +6,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	private int n;
+	private int count;
 	public UsrHomeController() {
-		n = 0;
+		count = 0;
 	}
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕";
 	}
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "잘가";
+	}
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public int showMain3() {
+		int a = 1;
+		int b = 2;
+		return a + b;
+	}
 	@RequestMapping("/usr/home/main4")
 	@ResponseBody
-	public int increaseNum() {
-		return n++;
+	public int showMain4() {
+		return count++;
 	}
-
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count변수 0으로 초기화됌";
+	}
+	
 }
