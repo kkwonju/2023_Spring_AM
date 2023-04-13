e95b6a4c7069b789b0a6a6c50870acbc93a6fad5
kkwonju <kkwo4140@gmail.com>
2023-04-13 15:32:20 +0900

    n 변수의 값 초기화 및 증가

--- src/main/java/com/KoreaIT/kkwo/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/kkwo/demo/controller/UsrHomeController.java
@@ -6,9 +6,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	private int n;
+	public UsrHomeController() {
+		n = 0;
+	}
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕";
 	}
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int increaseNum() {
+		return n++;
+	}
+
 }
