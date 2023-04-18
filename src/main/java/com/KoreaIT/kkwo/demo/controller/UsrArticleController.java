f1f36eed635d88b0891be4f5a13f2821bb7c04c5
kkwonju <kkwo4140@gmail.com>
2023-04-18 13:31:49 +0900

    getArticle에 ResultData 적용

--- src/main/java/com/KoreaIT/kkwo/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/kkwo/demo/controller/UsrArticleController.java
@@ -8,28 +8,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.KoreaIT.kkwo.demo.service.ArticleService;
+import com.KoreaIT.kkwo.demo.util.Ut;
 import com.KoreaIT.kkwo.demo.vo.Article;
+import com.KoreaIT.kkwo.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
 	@Autowired // 자동 연결
 	private ArticleService articleService;
-	
+
 //	public UsrArticleController() {
 //	}
-	
+
 	/*
-	위의 @Autowired 어노테이션은 Spring 프레임워크에서 제공하는 
-	의존성 주입(Dependency Injection) 기능을 사용하기 위한 것입니다. 
-	@Autowired가 붙은 필드에 대한 의존성 주입은 해당 클래스의 
-	인스턴스 생성 후, 스프링 컨테이너가 해당 클래스의 객체를 생성할 때 실행됩니다.
-
-	따라서, @Autowired로 주입되는 ArticleService 객체는 UsrArticleController 객체가
-	 생성된 이후에 주입됩니다. 그리고 생성자는 객체 생성 시에 실행되므로, 
-	 UsrArticleController의 생성자가 먼저 실행됩니다.
+	 * 위의 @Autowired 어노테이션은 Spring 프레임워크에서 제공하는 의존성 주입(Dependency Injection) 기능을
+	 * 사용하기 위한 것입니다.
+	 * 
+	 * @Autowired가 붙은 필드에 대한 의존성 주입은 해당 클래스의 인스턴스 생성 후, 스프링 컨테이너가 해당 클래스의 객체를 생성할 때
+	 * 실행됩니다.
+	 * 
+	 * 따라서, @Autowired로 주입되는 ArticleService 객체는 UsrArticleController 객체가 생성된 이후에
+	 * 주입됩니다. 그리고 생성자는 객체 생성 시에 실행되므로, UsrArticleController의 생성자가 먼저 실행됩니다.
 	 */
-	
-
 
 	@RequestMapping("/usr/article/delete")
 	@ResponseBody
@@ -62,20 +62,20 @@ public class UsrArticleController {
 		Article article = articleService.getArticleByInputedId(id);
 		return article;
 	}
-	
+
 	@RequestMapping("/usr/article/list")
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articleService.getArticles();
 	}
-	
-	@RequestMapping("/usr/article/article")
+
+	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticleByInputedId(id);
 		if (article == null) {
-			return String.format("%d번 글은 존재하지 않습니다", id);
+			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id));
 		}
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 글입니다", id), article);
 	}
 }
--- src/main/java/com/KoreaIT/kkwo/demo/util/Ut.java
+++ src/main/java/com/KoreaIT/kkwo/demo/util/Ut.java
@@ -20,5 +20,4 @@ public class Ut {
 	public static String f(String format, Object... args) {
 		return String.format(format, args);
 	}
-
 }
